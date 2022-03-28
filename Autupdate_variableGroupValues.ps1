6c5664736e7f8ff9eb62ba3f8cc6f8388664e174
Adinath-Dukare-382 <88125451+Adinath-Dukare-382@users.noreply.github.com>
2022-03-28 18:40:37 +0530

    Update Autupdate_variableGroupValues.ps1

--- Autupdate_variableGroupValues.ps1
+++ Autupdate_variableGroupValues.ps1
@@ -20,7 +20,7 @@ Param(
     [string]$group_id             
 )
 $ErrorActionPreference = "Stop"  
-$env:GithubToken_ENV_VAR = "qqfrwwltn55qaf6rluojxnb5jizsqcalfkkfwpbtezu6n6h6qa3q"
+$env:AZURE_DEVOPS_EXT_PAT = "qqfrwwltn55qaf6rluojxnb5jizsqcalfkkfwpbtezu6n6h6qa3q"
     $stores = Import-csv $excelfile
 
     foreach($data in $stores)
