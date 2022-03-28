9d2cc6b45a17ea6ca45c621d56208ab9be2f09e9
Adinath-Dukare-382 <88125451+Adinath-Dukare-382@users.noreply.github.com>
2022-03-28 16:36:13 +0530

    Add files via upload

---
+++ Autupdate_variableGroupValues.ps1
@@ -0,0 +1,33 @@
+﻿<#
+ .SYNOPSIS
+     .     
+ .EXAMPLE
+     PS .\UpdateVariableGroupVariables.ps1 -organization "allscriptshealthcare" -excelfile "C:\Users\R807600\rohan\allscripts\powershell_scripts\metaexcel.csv" -project "transaction-systems" -AzureDevOpsPAT "blgvexdinyylbzdbihrhfztgerxa2dwzjgz7jsy5qx24fypaqdja" -varvalue 'true' -group_id 3
+     Creates the metajson file
+ #>
+
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory = $True)]
+    [string]$organization,
+    [Parameter(Mandatory = $True)]
+    [string]$excelfile,
+    [Parameter(Mandatory = $True)]
+    [string]$project,
+    [Parameter(Mandatory = $True)]
+    [string]$AzureDevOpsPAT,
+    [Parameter(Mandatory = $True)]
+    [string]$varvalue,
+    [Parameter(Mandatory = $True)]
+    [string]$group_id             
+)
+$ErrorActionPreference = "Stop"  
+
+    $stores = Import-csv $excelfile
+
+    foreach($data in $stores)
+    {
+        $varname = $data.Build_Name
+        az pipelines variable-group variable update --group-id $group_id --name $varname --value "$varvalue" --org https://dev.azure.com/$organization --project $project
+
+    }
